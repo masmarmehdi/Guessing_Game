ca8024b3a0d1506b10b0fe1e175eab01bbd1989a
masmarmehdi <masmar507@gmail.com>
2021-01-16 21:20:09 +0200

    Game

---
+++ Guessing_Game.py
@@ -0,0 +1,20 @@
+import random
+number = random.randint(1,10)
+def guessing_number(n):
+            if n < number :
+                print("<<TOO LOW>>")
+            elif n > number :
+                print("<<TOO FAR>>")
+            elif n == number:
+                print("<<BINGOO!! YOU WON!")
+                return True
+
+
+def main():
+    print("NOTE: You have only 3 attempt to guess the right answer!!")
+    for i in range(3):
+        n = int(input("Guess a number between 1 and 10 : "))
+        if guessing_number(n) == True:
+            break
+
+main()
